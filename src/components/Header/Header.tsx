6f8665e465a8d508bef429699dcf4816dbda196c
JesusMeister <a01656699@tec.mx>
2024-05-05 21:54:40 -0600

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ function Header() {
   return (
     <div className='bg-gray-100 h-full w-full flex flex-wrap justify-evenly items-center p-5 border-b-2 border-gray-400'>
       <div><Link to = {ROUTES.HOME} className='font-bold sm:text-xl md:text-2xl lg:text-3xl p-4' onClick={()=>setPath("")}>PELIMEISTER</Link></div>
-      <div className='sm:text-base md:text-lg lg:text-xl space-x-4 flex justify-evenly items-center'>
+      <div className='sm:text-base md:text-lg lg:text-xl space-x-4 flex flex-wrap justify-evenly items-center'>
       <div><Link to = {ROUTES.HOME} className={classNames({'text-red-400': path === ROUTES.HOME})}>HOME </Link></div>
       <div><Link to = {`${ROUTES.TABLE}/favorites`} className={classNames({'text-red-400': path === `${ROUTES.TABLE}/favorites`})}>FAVORITES </Link></div>
       <div><Link to = {`${ROUTES.TABLE}/${tableRoutes.nowPlaying.name}`} className={classNames({'text-red-400': path === `${ROUTES.TABLE}/${tableRoutes.nowPlaying.name}`})}>NOW PLAYING </Link> </div>
