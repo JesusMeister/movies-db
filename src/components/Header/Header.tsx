9d1205ae454b90a1ca6e5204c425649f73adf360
JesusMeister <a01656699@tec.mx>
2024-05-05 20:41:25 -0600

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../routes/constants'
 import { tableRoutes } from '../../pages/Table/Table'
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 
 function Header() {
+  const [path , setPath] = useState<string>();
+  useEffect(()=>{
+    setPath(window.location.pathname);
+  }, [path])
   return (
     <div className='bg-gray-100 h-full w-full flex justify-evenly items-center p-5 border-b-2 border-gray-400'>
-      <Link to = {ROUTES.HOME} className='font-bold sm:text-xl md:text-2xl lg:text-3xl'>PELIMEISTER</Link>
+      <Link to = {ROUTES.HOME} className='font-bold sm:text-xl md:text-2xl lg:text-3xl' onClick={()=>setPath("")}>PELIMEISTER</Link>
       <p className='sm:text-base md:text-lg lg:text-xl space-x-4'>
-      <Link to = {ROUTES.HOME}>HOME </Link>
-      <Link to = {`${ROUTES.TABLE}/favorites`}>FAVORITES </Link>
-      <Link to = {`${ROUTES.TABLE}/${tableRoutes.nowPlaying.name}`}>NOW PLAYING </Link>
-      <Link to = {`${ROUTES.TABLE}/${tableRoutes.popular.name}`}>POPULAR </Link>   
-      <Link to = {`${ROUTES.TABLE}/${tableRoutes.topRated.name}`}>TOP RATED </Link>          
+      <Link to = {ROUTES.HOME} className={classNames({'text-red-400': path === ROUTES.HOME})} onClick={()=>setPath("")}>HOME </Link>
+      <Link to = {`${ROUTES.TABLE}/favorites`} className={classNames({'text-red-400': path === `${ROUTES.TABLE}/favorites`})} onClick={()=>setPath("")}>FAVORITES </Link>
+      <Link to = {`${ROUTES.TABLE}/${tableRoutes.nowPlaying.name}`} className={classNames({'text-red-400': path === `${ROUTES.TABLE}/${tableRoutes.nowPlaying.name}`})} onClick={()=>setPath("")}>NOW PLAYING </Link> 
+      <Link to = {`${ROUTES.TABLE}/${tableRoutes.popular.name}`} className={classNames({'text-red-400': path === `${ROUTES.TABLE}/${tableRoutes.popular.name}`})} onClick={()=>setPath("")}>POPULAR </Link>   
+      <Link to = {`${ROUTES.TABLE}/${tableRoutes.topRated.name}`} className={classNames({'text-red-400': path === `${ROUTES.TABLE}/${tableRoutes.topRated.name}`})} onClick={()=>setPath("")}>TOP RATED </Link>          
       </p>
     </div>
   )
