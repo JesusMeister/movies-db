803dfae9532cdc578438e92296c4bde04b28f499
JesusMeister <a01656699@tec.mx>
2024-05-05 19:03:43 -0600

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { tableRoutes } from '../../pages/Table/Table'
 function Header() {
   return (
     <div className='bg-gray-100 h-full w-full flex justify-evenly items-center p-5 border-b-2 border-gray-400'>
-      <p className='font-bold sm:text-xl md:text-2xl lg:text-3xl'>PELIMEISTER</p>
+      <Link to = {ROUTES.HOME} className='font-bold sm:text-xl md:text-2xl lg:text-3xl'>PELIMEISTER</Link>
       <p className='sm:text-base md:text-lg lg:text-xl space-x-4'>
       <Link to = {ROUTES.HOME}>HOME </Link>
       <Link to = {`${ROUTES.TABLE}/favorites`}>FAVORITES </Link>
